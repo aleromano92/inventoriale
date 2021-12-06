06df48ca839224cbdca8187533dd3efda6d2cde3
Alessandro Romano <alessandro.romano1992@gmail.com>
2021-12-06 01:36:39 +0100

    try update

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -29,7 +29,7 @@ const AppHome: NextPage = () => {
             onClick={scanBarcode}
             className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
           >
-            Scanna il barcode
+            Scanna
           </button>
           {shouldScanBarcode && (
             <BarcodeScannerComponent
