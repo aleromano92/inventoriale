838eabf253423c26babccf7c607eaf77c15c29e8
Alessandro Romano <alessandro.romano1992@gmail.com>
2021-12-06 22:22:03 +0100

    adds link to main screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
@@ -15,20 +15,10 @@ const Home: NextPage = () => {
         <h1 className="font-bold">
           Welcome to <a href="https://nextjs.org">Inventoriale!</a>
         </h1>
+        <section>
+          <Link href="/app">Vai all'app</Link>
+        </section>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
